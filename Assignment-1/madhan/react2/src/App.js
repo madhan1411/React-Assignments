a3165e0992786b85afab59e1de75fa22effe6d2b
madhan1411 <66825748+madhan1411@users.noreply.github.com>
2022-04-04 14:58:16 +0530

    assignment-1

---
+++ Assignment-1/madhan/react2/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div> <div id="root">
+      <div className="main-container">
+        <div className="container-1">
+          <div className="container-1a"></div>
+          <div className="container-1b"></div>
+        </div>
+        <div className="container-2"></div>
+        <div className="conatiner-3">
+          <div className="container-3a"></div>
+          <div className="container-3b"></div>
+          <div className="container-3c"></div>
+        </div>
+      </div>
+    </div></div>
+  </div>
+  );
+}
+
+export default App;
---
+++ Assignment-1/react1/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.html'
+
+function App() {
+  return (
+    <div className="null">
+     <div className="top">
+     <div className="yellow"></div>
+     <div className="yellow"></div>
+     </div>
+     <div className="mid">
+     <div className="red"></div>
+     </div>
+     <div className="bottom">
+     <div className="blue"></div>
+     <div className="blue"></div>
+     <div className="blue" id="blocked"></div>
+     </div>
+     </div>);
+}
+
+export default App;
