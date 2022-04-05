74d07063f95fc2c5a892be681920ccbe0183bc67
madhan1411 <66825748+madhan1411@users.noreply.github.com>
2022-04-05 11:43:50 +0530

    assignment-2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 
 
 function App(props) {
  const {employee}=props;
+ const myfunc=(label,value)=>{
+   return(
+     <div className='field-container'>
+       <label>{label}</label>
+       <div>{value}</div>
+     </div>
+   );
+ }
     
 return(
-  <div className="container">
+  <div className="App">
+  <div className='card-container'>
+  <img src={employee.profileImg} width='100%'/>
+  <h2>{employee.name}</h2>
+  {myfunc('location',employee.location)}
+  {myfunc('bloodGroup',employee.location)}
+  {myfunc('age',employee.age)}
   {/* Start your React code here */}
+</div>
 </div>
 )
 }
